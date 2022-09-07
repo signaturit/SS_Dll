1921c493e662e38d346963fc3ad48cd369fe94b3
Raquel Fontanal <rfontanal@ivnosys.com>
2022-09-07 16:17:36 +0200

    Add project files.

---
+++ SS/Login.cs
@@ -0,0 +1,152 @@
+﻿using HtmlAgilityPack;
+using ProcessorsUtilities;
+using ProcessorsUtilities.Model;
+using ProcessorsUtilities.Utils;
+using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SS
+{
+    public class Login
+    {
+        private string baseURL;
+        private string loginURL;
+        private CertType type = CertType.NORMAL;
+        private HttpUtilsRq utilsRq = new HttpUtilsRq();
+        private CError ctlErr;
+
+        /// <summary>
+        /// Contains all the information of the generated error. Adds information to the exception thrown by the class if it is a handled exception.
+        /// </summary>
+        public CError Error
+        {
+            get { return ctlErr; }
+        
+        }
+
+        #region Init
+        /// <summary>
+        /// Set the basic connection parameters to be able to start the login process
+        /// </summary>
+        /// <param name="idService"></param>
+        /// <param name="certType"></param>
+        /// <param name="urlBase"></param>
+        /// <param name="connectorURL">LoginUrl of the connector</param>
+        /// <param name="ktoken"></param>
+        /// <param name="certid"></param>
+        /// <param name="pin"></param>
+        /// <param name="entorno"></param>
+        /// <param name="sessionId"></param>
+        /// <param name="brokerUrl"></param>
+        /// <param name="dllVersion"></param>
+        public Login(string idService, CertType certType, string urlBase,string connectorURL, string ktoken, string certid, string pin, string entorno, string sessionId, string brokerUrl, double dllVersion)
+        {
+            baseURL = urlBase;
+            loginURL = connectorURL;
+            utilsRq.SetParams(certType, ktoken, certid, pin, entorno);
+            utilsRq.StartTracer(sessionId, brokerUrl, new Uri(baseURL).Host, dllVersion);
+            utilsRq.SetService(idService);
+            if (certType == CertType.IVSIGN)
+                Common.IvSignOK(entorno, ktoken, certid, pin);
+        }
+
+        /// <summary>
+        /// Set the basic connection parameters to be able to start the login process
+        /// </summary>
+        /// <param name="utilsRequest"></param>
+        /// <param name="certType"></param>
+        /// <param name="urlBase"></param>
+        public Login(HttpUtilsRq utilsRequest, CertType certType, string urlBase)
+        {
+            utilsRq = utilsRequest;
+            type = certType;
+            baseURL = urlBase;
+        }
+
+        #endregion Init
+
+        #region Login
+        /// <summary>
+        /// Performs the login process on the platform website
+        /// </summary>
+        /// <param name="clientCertificate">Certificate with which to login the platform website</param>
+        /// <param name="cookies"></param>
+        /// <param name="referURL">In this connector the first referUrl is harcoded, so the possibility of passing it externally is maintained</param>
+        /// <returns></returns>
+        public string Process(X509Certificate2 clientCertificate, ref CookieContainer cookies, string referURL = "")
+        {
+            AIRequests aIRequests = utilsRq.GetAiRequest(clientCertificate, 5000000, baseURL, type);
+            aIRequests = Process(aIRequests, referURL);
+            cookies = aIRequests.CookieContainer;
+            return aIRequests.HtmlContent;
+
+        }
+
+        /// <summary>
+        /// Performs the login process on the platform website
+        /// </summary>
+        /// <param name="aIRequests"></param>
+        /// <param name="referURL">In this connector the first referUrl is harcoded, so the possibility of passing it externally is maintained</param>
+        /// <returns></returns>
+        public AIRequests Process(AIRequests aIRequests, string referURL = "")
+        {
+            string postdata = null;
+            string requestURL = loginURL;
+
+            aIRequests.HttpRequest(ref requestURL, ref referURL, AccessType.GET);
+            string content = aIRequests.HtmlContent.ToLower();
+
+            //Old error handling. As it is not possible to determine in which element of the html each message is, the check is kept on all the content.
+            if (content.Contains("se requiere un certificado de usuario aceptado por la Seguridad Social.".ToLower()))
+                ThrowCustomError(new CException(Code.NO_SEL_CERT_CORRECTLY, "Se requiere un certificado de usuario aceptado por la Seguridad Social."), code: Code.NO_SEL_CERT_CORRECTLY);
+
+            else if (content.Contains("Acceso no autorizado".ToLower()) && content.Contains("en la lista de los certificados admitidos por la Seguridad Social".ToLower()))
+            {
+                ThrowCustomError(new CException(Code.BAD_CERTIFICATE, "El certificado que usted está utilizando no esta incluido en la lista de los certificados admitidos por la Seguridad Social. Por favor, consulte la sección de Certificados Digitales de la Sede."), code: Code.BAD_CERTIFICATE);
+            }
+            else if (content.Contains("sica no figura en la base de datos"))
+                ThrowCustomError(new CException(Code.USER_NOT_REGISTERED, "Usuario no registrado"), code: Code.USER_NOT_REGISTERED);
+            else if (content.Contains("sica figura con nombre diferente"))
+                ThrowCustomError(new CException(Code.USER_NOT_REGISTERED, "Usuario no registrado"), code: Code.USER_NOT_REGISTERED);
+            else if (content.Contains("no se encuentra suscrito a"))
+                ThrowCustomError(new CException(Code.USER_NOT_SUSCRIBED, "Usuario no suscrito"), code: Code.USER_NOT_SUSCRIBED);
+            else if (content.Contains("cerrada temporalmente"))
+                ThrowCustomError(new CException(Code.PLATFORM_UNAVAILABLE, "Plataforma cerrada temporalmente"), code: Code.PLATFORM_UNAVAILABLE);
+            else if (content.Contains("no se encuentra disponible"))
+                ThrowCustomError(new CException(Code.PLATFORM_UNAVAILABLE, "Plataforma no disponible temporalmente"), code: Code.PLATFORM_UNAVAILABLE);
+            else if (content.Contains("revoked"))
+                ThrowCustomError(new CException(Code.BAD_CERTIFICATE, "Certificado revocado"), code: Code.BAD_CERTIFICATE);
+            else if (content.Contains("no se puede atender en este momento el servicio".ToLower()))
+                ThrowCustomError(new CException(Code.PLATFORM_UNAVAILABLE), code: Code.PLATFORM_UNAVAILABLE);
+            else if (content.Contains("SVL00001".ToLower()))
+                ThrowCustomError(new CException(Code.USER_NOT_REGISTERED, "Usuario no registrado"), code: Code.USER_NOT_REGISTERED);
+
+            string postData = "&SPM.CONTEXT=internet&SPM.HAYJS=1&SPM.ISPOPUP=0&SPM.PORTALTYPE=HTML&tipoCodigoDestinatario=01&codigoDestinatario=06017078W&SPM.ACC.BUSCAR=Buscar";
+            HtmlNode ticket = aIRequests.HtmlDocument.GetNode(string.Empty, "//input[@id='ARQ.SPM.TICKET']", true);
+            if (ticket != null)
+            {
+                postData = "ARQ.SPM.TICKET=" + ticket.GetAttr("value") + postdata;
+            }
+            requestURL = "https://w2.seg-social.es/ProsaInternet/OnlineAccess";
+            aIRequests.HttpRequest(ref requestURL, ref referURL, AccessType.POST, postData);
+
+            return aIRequests;
+        }
+        #endregion Login
+
+        #region Errors
+        /// <summary>
+        ///Generates a CError type error with all the manually generated exception information, and throws the exception itself.
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="exMsg"></param>
+        /// <param name="code"></param>
+        private void ThrowCustomError(Exception ex, string exMsg = "", Code code = Code.PLATFORM_ERROR)
+        {
+            ctlErr = new CError(ex, (string.IsNullOrEmpty(exMsg) ? ex.Message : exMsg), code);
+            throw ctlErr.Ex;
+        }
+        #endregion Errors
+    }
+}
---
+++ SS/Readme_IvWebRequest.txt
@@ -0,0 +1,130 @@
+﻿//==== IvWebRequest ====
+//Performs web request and use an IvSign certificate as Client Certificate on Authentication is Required
+
+//With the same instance, Cookies are stored and can be retrieved from:
+
+
+//=== Instantiate ===
+//To instantiate WebClient, you can do it WITH or WITHOUT IvSign Object 
+//WebClient that requires Login in IvSign
+var WebClient = new IvWebRequest(new IvSignInit() { server = "ivnosys.net", userid = "user1", pass = "password1" });
+//or web client that use existing Token
+var WebClient = new IvWebRequest(new IvSignInit() { server = "ivsign.net", token = "KpzkBdM92nujnG5fQLNfkYsbVuMG95R/NppD85v65XcCgl6xw51wRYzoJW1AJhQ5s" });
+//or use basic WebClient without using IvSign
+var WebClient = new IvWebRequest();
+
+//After instanced, the "error" string can be set if some error occurs (!string.IsNullOrWhiteSpace(WebClient.error))
+
+//The CertObject needs at least Certid and pin
+var cert = new CertObj()  { certid = "8F8F8F8F8F8F", pin = "123456"};
+//But its better performance if you call it with public cert
+var cert = new CertObj()  { certid = "8F8F8F8F8F8F", pin = "123456", pubCer = Convert.FromBase64String("Get from base64 or directly from x509") };
+
+//=== Disposing ===
+//It is recommended to dispose the instance when not necesary
+
+//=== Headers ===
+//The headers Dictionary can be null or filled in this way:
+var header = new Dictionary<string, string>() { { "Authentication", "Authentication value" }, { "User-Agent", "Your-User Agent" } };
+//even, you can add values conditionally
+if (x != y && !header.Contains("name") ) header.Add("name","value");
+
+//=== WebForm POST parameters ===
+//The formdata Dictionary can be null or filled in this way:
+var data = new Dictionary<string, string>() { { "Key1", "Value1" }, { "Key2", "Value2" } };
+//even, you can add values conditionally
+if (x != y) data.Add("Conditionalkey","ConditionalValue");
+
+
+//=== Methods ===
+//For direct GET method, only 3 parameters are allowed: 
+get(url [, CertObj] [, headers]); last two are optional
+
+//There are 2 direct POST methods:
+ //POST body or basic webform 
+ byte[] post(string url, CertObj in_cert = null, Dictionary<string, string> headers = null, Dictionary<string, string> formdata = null, string body = null)
+ 
+ //POST extended webform with file uploading
+ public byte[] postwebform(string url, CertObj in_cert = null, Dictionary<string, string> headers = null, Dictionary<string, string> formdata = null, string FileField = "", string FileName = "", Stream FileStream = null, string ContentType = "")
+
+ //There are a generic method to perform requests:
+ public byte[] request(string url, string method, CertObj in_cert = null, Dictionary<string, string> headers = null, Dictionary<string, string> formdata = null, string body = null, string FileField = "", string FileName = "", Stream FileStream = null, string ContentType = "")
+
+// === Cookies ===
+// Cookies are stored for each domain in class IvWebRequest.
+// To get list of domains with cookies use:
+ List<string> Get_Cookies_Domains(); 
+// This function returns a list of domains. To get cookies for each domain, use: 
+List<Cookie> Get_Cookies(string url);
+//This function returns a Cookie Object for specific Url
+
+ /* C# EXAMPLES */
+//WITHOUT CLIENT CERTIFICATE AUTHENTICATION
+//Basic GET usage overriding header 
+var web1 = new IvWebRequest();
+if (!string.IsNullOrWhiteSpace(web1.error))
+	Console.WriteLine(web1.error);
+var resp1 = web1.get("https://ivsign.net/Keyman/rest/v2/cert/list?request.cert.userid=pruiz", null, new Dictionary<string, string>() { { "Authentication", "K9kYbxu8aiR697W1IZLq1CO9AXmYwr/UdLq1MaueeDdm0IJAUoj/vcHOWyVhgztqm" }, { "User-Agent", "Your-User Agent" } });
+if (!string.IsNullOrWhiteSpace(web1.error))
+	Console.WriteLine(web1.error);
+if (resp1 != null)
+	Console.WriteLine(Encoding.UTF8.GetString(resp1));
+web1.close();
+
+//Basic POST usage adding new header
+var web11 = new IvWebRequest();
+if (!string.IsNullOrWhiteSpace(web11.error))
+	Console.WriteLine(web11.error);
+var resp11 = web11.post("https://ivsign.net/Keyman/rest/v4/user/get", null, new Dictionary<string, string>() { { "Authentication", "K9kYbxu8aiR697W1IZLq1CO9AXmYwr/UdLq1MaueeDdm0IJAUoj/vcHOWyVhgztqm" }, { "Content-Type", "application/json" } }, null, "{\"user\":{\"userid\":\"ktest\"}}");
+if (!string.IsNullOrWhiteSpace(web11.error))
+	Console.WriteLine(web11.error);
+if (resp11 != null)
+	Console.WriteLine(Encoding.UTF8.GetString(resp11));
+web11.close();
+
+//POST page with form values
+var web4 = new IvWebRequest();
+if (!string.IsNullOrWhiteSpace(web4.error))
+	Console.WriteLine(web4.error);
+var resp4 = web4.post("http://127.0.0.1/prueba.php", null, new Dictionary<string, string>() { { "Authentication", "K9kYbxu8aiR697W1IZLq1CO9AXmYwr/UdLq1MaueeDdm0IJAUoj/vcHOWyVhgztqm" } }, new Dictionary<string, string>() { { "key1", "value1" }, { "key2", "value2" } });
+if (!string.IsNullOrWhiteSpace(web4.error))
+	Console.WriteLine(web4.error);
+if (resp4 != null)
+	Console.WriteLine(Encoding.UTF8.GetString(resp4));
+web4.close();
+
+			
+//WITH CLIENT CERTIFICATE AUTHENTICATION
+//GET webpage with IvSign Certificate and Application token
+var ivsign2 = new IvSignInit() { server = "ivsign.net", token = "KpzkBdM92nujnG5fQLNfkYsbVuMG95R/NppD85v65XcCgl6xw51wRYzoJW1AJhQ5s" };
+var web2 = new IvWebRequest(ivsign2);
+if (!string.IsNullOrWhiteSpace(web2.error))
+	Console.WriteLine(web2.error);
+var resp2 = web2.get("https://tools.ivnosys.net/cert/check", new CertObj() { certid = "8F8F8F8F8F8F", pin = "123456", pubCer = Convert.FromBase64String("Get from base64 or directly from x509") });
+if (!string.IsNullOrWhiteSpace(web2.error))
+	Console.WriteLine(web2.error);
+if (resp2 != null)
+	Console.WriteLine(Encoding.UTF8.GetString(resp2));
+web2.close();
+
+//GET page with IvSign Certificate and Login with User/password
+var ivsign3 = new IvSignInit() { server = "ivnosys.net", userid = "user1", pass = "pass1" };
+var web3 = new IvWebRequest(ivsign3);
+if (!string.IsNullOrWhiteSpace(web3.error))
+	Console.WriteLine(web3.error);
+var cert = new CertObj()  { certid = "8F8F8F8F8F8F", pin = "123456", pubCer = Convert.FromBase64String("Get from base64 or directly from x509") };
+var resp3 = web3.get("https://tools.ivnosys.net/cert/check", cert);
+if (!string.IsNullOrWhiteSpace(web3.error))
+	Console.WriteLine(web3.error);
+if (resp3 != null)
+	Console.WriteLine(Encoding.UTF8.GetString(resp3));
+//Al reutilizar la instancia, se puede hacer otra llamada sin volver a pasarle los datos del certificado
+var resp31 = web3.get("https://apsc.accv.es/utiles/frontal/comprobacionaccv/comprobacionaccv.htm");
+if (!string.IsNullOrWhiteSpace(web3.error))
+	Console.WriteLine(web3.error);
+if (resp31 != null)
+	Console.WriteLine(Encoding.UTF8.GetString(resp31));
+web3.close();
+
+
+			
